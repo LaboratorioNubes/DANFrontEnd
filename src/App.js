e1f4e562ed06876ee1f3bb70a2a6e0c520ac3e18
exesanchz <exequiel_2080@hotmail.com>
2021-10-10 18:16:57 -0300

    refactor Sign In

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import SignIn from "./containers/SignIn/SignIn.js";
 import SignUp from "./containers/SignUp/SignUp";
 import HomeView from "./containers/HomeView/HomeView";
 import Dashboard from "./containers/Dashboard/Dashboard";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <SignIn />
+          <Route path="/signIn">
+            <SignIn />
+          </Route>
           <Route path="/signUp">
             <SignUp />
           </Route>
           <Route path="/dashboard">
             <Dashboard />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/signIn" />
           </Route>
         </header>
       </div>
--- src/containers/SignIn/SignIn.js
+++ src/containers/SignIn/SignIn.js
@@ -1,7 +1,9 @@
-import {React, useState} from "react";
-import { Container, Link } from "@material-ui/core";
+import { React, useState } from "react";
+import { Container, Link, Button, makeStyles } from "@material-ui/core";
 import CustomInput from "../../components/CustomInput/CustomInput.js";
 import LoginButton from "../../components/LoginButton/Button.js";
+//import BuildCircleIcon from '@mui/icons-material/BuildCircle';
+import ConstructionRoundedIcon from "@mui/icons-material/ConstructionRounded";
 import { useHistory } from "react-router-dom";
 
 /*const loginInicial = {
@@ -9,8 +11,34 @@ import { useHistory } from "react-router-dom";
   password: "",
 };*/
 
+const useStyles = makeStyles((theme) => ({
+  signInTitleElements: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems:"center"
+  },
+  signInTitle: {
+    color: "#3f51b5",
+    marginBottom: "0rem",
+    marginTop: "0rem"
+  },
+  signInSubTitle: {
+    marginTop: "0.5rem",
+    backgroundColor: "#3f51b5",
+  },
+  buildIcon: {
+    paddingLeft: "1rem",
+    color: "#3f51b5",
+    fontSize: "2.5rem!important",
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const SignIn = ({}) => {
-  const [signIn,setLogin] = useState();
+  const [signIn, setLogin] = useState();
+  const classes = useStyles();
 
   /*const actualizarLogin = (atributo,valor) => {
         const clienteNuevo = {...cliente, [atributo]: valor};
@@ -28,18 +56,21 @@ const SignIn = ({}) => {
   };
 
   const handleEmailChange = (e) => {
-    setLogin({...signIn,
-      email: e.target.value});
+    setLogin({ ...signIn, email: e.target.value });
   };
 
   const handlePasswordChange = (e) => {
-    setLogin({...signIn,
-      password: e.target.value});
+    setLogin({ ...signIn, password: e.target.value });
   };
 
   return (
     <Container maxWidth="xs">
       <form className="form">
+        <div className={classes.signInTitleElements}>
+          <h2 className={classes.signInTitle}>NEXO</h2>
+          <ConstructionRoundedIcon className={classes.buildIcon} />
+        </div>
+        <h3 className={classes.signInSubTitle}> Construction Materials </h3>
         <CustomInput
           labelText="Email"
           id="email"
@@ -61,18 +92,15 @@ const SignIn = ({}) => {
 
         <LoginButton
           type="button"
-          color="primary"
+          color="#3f51b5"
+          style={{ backgroundColor: "#3f51b5" }}
           className="form__custom-button"
           onClick={dashboardRedirect}
         >
           Log in
         </LoginButton>
       </form>
-      <Link
-        component="button"
-        variant="body2"
-        onClick={signUpRedirect}
-      >
+      <Link component="button" variant="body2" onClick={signUpRedirect}>
         Not an acount yet? Join us now!
       </Link>
     </Container>
