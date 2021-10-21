6b1c62b13e6665958166ccd12ce39390d6659e4e
exesanchz <exequiel_2080@hotmail.com>
2021-10-21 01:16:58 -0300

    add get de pagos

--- src/components/Deposits/Deposits.js
+++ src/components/Deposits/Deposits.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Title from '../Title/Title';
+import axios from "axios";
+import PropTypes from "prop-types";
+import * as microserviceActions from "../../actions/microserviceActions";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 function preventDefault(event) {
   event.preventDefault();
@@ -14,25 +19,50 @@ const useStyles = makeStyles({
   },
 });
 
-const Deposits = () => {
+const Deposits = (props) => {
   const classes = useStyles();
+  useEffect(() => {
+    axios
+      .get(`http://localhost:9005/api/pago/1`)
+      .then((resp) => {
+        console.log(resp.data);
+        props.microserviceActions.setPayments(resp.data);
+      });
+  });
   
   return (
-    <React.Fragment>
+    <>
       <Title>Recent Payments</Title>
       <Typography component="p" variant="h4">
-        $3,024.00
+        ${props.payments && props.payments.length > 0 ? props.payments[0].monto : 0}
       </Typography>
       <Typography color="textSecondary" className={classes.depositContext}>
-        on 15 October, 2021
+      {props.payments && props.payments.length > 0 ? props.payments[0].fechaPago : "on 15 October, 2021"}
       </Typography>
       <div>
         <Link color="primary" href="#" onClick={preventDefault}>
           View balance
         </Link>
       </div>
-    </React.Fragment>
+    </>
   );
 }
 
-export default Deposits;
+Deposits.propTypes = {
+  payments: PropTypes.array,
+  microserviceActions: PropTypes.object,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    payments: state.payments.payments.sort((a, b) => b.date - a.date),
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    microserviceActions: bindActionCreators(microserviceActions, dispatch),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Deposits);
